8acfad3bb4c8c1b60a4f70344ea0faa0a918a44a
Derrick Ho <derrickho@Derricks-MacBook-Pro.local>
2016-12-29 10:44:18 -0800

    Initial Commit

---
+++ SwiftyNotificationFramework/SwiftyNotificationFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyNotificationFramework.h
+//  SwiftyNotificationFramework
+//
+//  Created by Derrick Ho on 12/29/16.
+//  Copyright © 2016 Derrick Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyNotificationFramework.
+FOUNDATION_EXPORT double SwiftyNotificationFrameworkVersionNumber;
+
+//! Project version string for SwiftyNotificationFramework.
+FOUNDATION_EXPORT const unsigned char SwiftyNotificationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyNotificationFramework/PublicHeader.h>
+
+
